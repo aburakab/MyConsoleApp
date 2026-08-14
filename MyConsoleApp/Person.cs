e6ec1192451c9e72bd38a06b30a4f7bdf94357a5
Mohammad Aljaber <aburakab@gmail.com>
2026-08-14 17:56:22 +0300

    Remove LICENSE.txt file + Adding Person Changes

--- MyConsoleApp/Person.cs
+++ MyConsoleApp/Person.cs
@@ -1,6 +1,7 @@
 public class Person
 {
     public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
     public int Age { get; set; }
+    public string LastName { get; set; } = string.Empty;
 }
